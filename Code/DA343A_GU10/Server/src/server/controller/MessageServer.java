9a7afc7d8d8053b817f28990938998667b52500a
GabrielModin <gabriel.barzen@mailfence.com>
2021-02-26 17:50:37 +0100

    Base class structure created

---
+++ Code/DA343A_GU10/Server/src/server/controller/MessageServer.java
@@ -0,0 +1,11 @@
+package server.controller;
+
+public class MessageServer {
+    public static void main(String[] args) {
+        int port = 1089;
+        MessageServer client = new MessageServer(port);
+    }
+    MessageServer(int port){
+
+    }
+}
