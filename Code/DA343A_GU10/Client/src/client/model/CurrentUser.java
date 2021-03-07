3c5da995e3cb8598139e54a8c541bb62a1be9a32
Anton den mäktige <anton.holm.y@gmail.com>
2021-03-07 23:13:57 +0100

    Added metods to save and read contacts to files/*.dat file

--- Code/DA343A_GU10/Client/src/client/model/CurrentUser.java
+++ Code/DA343A_GU10/Client/src/client/model/CurrentUser.java
@@ -3,6 +3,7 @@ package client.model;
 
 import shared.User;
 
+import java.io.*;
 import java.util.LinkedList;
 
 public class CurrentUser {
@@ -22,6 +23,45 @@ public class CurrentUser {
     public void removeContact(User user) {
         if(user != null) {
             contacts.remove(user);
+        }
+    }
+
+    public boolean writeContactsToFile() {
+        String filepath = "files/contact_list_" + user.getName() + ".dat";
+        boolean contactsSaved = false;
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filepath))) {
+            oos.writeInt(contacts.size());
+
+            for (User contact : contacts) {
+                oos.writeObject(contact);
+                oos.flush();
+            }
+
+            contactsSaved = true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return contactsSaved;
+    }
+
+
+    public void readContactsFromFile() {
+        String filepath = "files/contact_list_" + user.getName() + ".dat";
+        User readContact;
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filepath))) {
+            int numbOfContacts = ois.readInt();
+
+            for (int i = 0; i < numbOfContacts; i++) {
+                readContact = (User) ois.readObject();
+                contacts.add(readContact);
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
